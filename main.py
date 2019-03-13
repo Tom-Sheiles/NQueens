74be318857a11c53950c6fe49607aa61c31d241a
Tom Sheiles <36983461+Tom-Sheiles@users.noreply.github.com>
2019-03-13 11:30:17 +1000

    Finished verificantion algorithm
    program now outputs valid answers for the n queens problem

--- main.py
+++ main.py
@@ -2,38 +2,51 @@ from queue import *
 from collections import deque
 import time
 
+
 # TODO: finish this function and then add a print function to print the results
 def validate_position(position):
 
     for i in range(n):
-        for j in range(1, n):
+        if position[i] == -1:
+            return
+
+    for i in range(n):
+        for j in range(i + 1, n):
             if position[i] == position[j]:
                 return
-            else:
-                solutions.append(position)
+
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                distance = (position[j] - position[i]) / (j - i)
+                if distance == 1 or distance == -1:
+                    return
+
+    solutions.append(position.copy())
 
 
-
-def create_Queue(start, n, startTime):
+def create_Queue(start, n):
 
     nodeQueue = deque([start])
     currentRow = 0
     currentPower = n
+    flag = 0
 
     while nodeQueue:
         if currentRow == n:
-            break
+            flag = 1
         currentNode = nodeQueue.popleft()
         validate_position(currentNode)
         nextRow = currentNode.copy()
 
-        for i in range(n):
-            nextRow[currentRow] = i
-            nodeQueue.append(nextRow.copy())
-            currentPower -= 1
-        if currentPower == 0:
-            currentRow += 1
-            currentPower = n ** (currentRow + 1)
+        if flag == 0:
+            for i in range(n):
+                nextRow[currentRow] = i
+                nodeQueue.append(nextRow.copy())
+                currentPower -= 1
+            if currentPower == 0:
+                currentRow += 1
+                currentPower = n ** (currentRow + 1)
     print("done")
 
 
@@ -43,11 +56,11 @@ if n > 8:
     print("n is too high to be calculated using BFS")
     exit()
 
-startState = [-1] * n
+startState = [0] * n
 solutions = []
 
 start = time.time()
-create_Queue(startState, n, start)
+create_Queue(startState, n)
 end = time.time()
 
 executeTime = end - start
